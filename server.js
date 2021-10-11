6162ec5f58e3e5f345e84dbcdc1be148dc193530
carlosandrescardenas <andres2158@hotmail.com>
2021-10-11 11:42:10 -0500

    change the port and add the mysql connection

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-//var mysql = require("mysql");
-//var connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-//  user     : process.env.DB_USER || 'applicationuser',
-//  password : process.env.DB_PASS || 'applicationuser',
-//  database : process.env.DB_NAME || 'movie_db'
-//});
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+ host     : process.env.DB_HOST || '192.168.55.7',
+ user     : process.env.DB_USER || 'root',
+ password : process.env.DB_PASS || 'root',
+ database : process.env.DB_NAME || 'movie_db'
+});
 
-//connection.connect();
+connection.connect();
 
 //function getMovies(callback) {    
 //        connection.query("SELECT * FROM movie_db.movies",
@@ -89,6 +89,6 @@ app.get('/pending', function(req, res){
   res.json(pending);
 })
 console.log("server listening through port: "+process.env.PORT);
-// Launch our API Server and have it listen on port 3000.
-app.listen(process.env.PORT || 3000);
+// Launch our API Server and have it listen on port 8085.
+app.listen(process.env.PORT || 8085);
 module.exports = app;

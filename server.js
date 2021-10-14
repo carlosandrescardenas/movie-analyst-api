b1502c57e48f58aed4447ee94c24fd93e19935b6
carlosandrescardenas <andres2158@hotmail.com>
2021-10-14 10:35:51 -0500

    fix space

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 var mysql = require("mysql");
 var connection = mysql.createConnection({
- host     : process.env.DB_HOST ,
- user     : process.env.DB_USER ,
- password : process.env.DB_PASS ,
+ host     : process.env.DB_HOST,
+ user     : process.env.DB_USER,
+ password : process.env.DB_PASS,
  database : process.env.DB_NAME
 });
 

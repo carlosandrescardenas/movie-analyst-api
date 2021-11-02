96c2faeae9531eb51c4ee94e87d616957d2de11d
carlosandrescardenas <andres2158@hotmail.com>
2021-11-02 10:07:05 -0500

    update mysql scripts

--- data_model/table_creation_and_inserts.sql
+++ data_model/table_creation_and_inserts.sql
@@ -1,8 +1,3 @@
-DROP DATABASE IF EXISTS movie_db;
-CREATE DATABASE movie_db;
-USE movie_db;
-GO
-
 CREATE TABLE movie_db.publication (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(21));
 CREATE TABLE movie_db.reviewer (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(255), publication VARCHAR(255), FOREIGN KEY (publication) REFERENCES publication(name) ON DELETE CASCADE);
 CREATE TABLE movie_db.moviereview (title VARCHAR(255) PRIMARY KEY, `release` VARCHAR(255), score INTEGER, reviewer VARCHAR(255), FOREIGN KEY (reviewer) REFERENCES reviewer(name) ON DELETE CASCADE);
